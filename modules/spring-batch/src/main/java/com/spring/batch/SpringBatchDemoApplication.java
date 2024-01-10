92d1bd41a7790ce5760808d1d87e1770ca9ba3ab
arunendra@raapid.ai-at-327636184371 <arunendra@raapid.ai>
2024-01-10 21:43:21 +0530

    working module structure

--- modules/spring-batch/src/main/java/com/spring/batch/SpringBatchDemoApplication.java
+++ modules/spring-batch/src/main/java/com/spring/batch/SpringBatchDemoApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBatchApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchApplication.class, args);
+public class SpringBatchDemoApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBatchDemoApplication.class, args);
 	}
-
 }
