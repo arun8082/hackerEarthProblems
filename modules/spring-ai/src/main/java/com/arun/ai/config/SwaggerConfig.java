5ebac0843398bddf12bc3997bf8859654d9feb83
Arun <arunendrakumar1996@gmail.com>
2025-03-03 20:29:01 +0530

    spring ai demo with prompt template
    (cherry picked from commit 871faf3b2e14248c973272a00fe78778342ccb9c)

---
+++ modules/spring-ai/src/main/java/com/arun/ai/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.arun.ai.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+
+}
---
+++ modules/spring-ai/src/main/java/com/arun/ai/dto/BookDetail.java
@@ -0,0 +1,13 @@
+package com.arun.ai.dto;
+
+import lombok.Data;
+
+@Data
+public class BookDetail {
+    private String category;
+    private String book;
+    private String year;
+    private String review;
+    private String author;
+    private String summary;
+}
